c5d9549a7bf284b1f5e0b0e2210371471f1a48e5
karol1ch <karol.chomoncik@gmail.com>
2020-05-22 09:34:27 +0200

    Clock bean configuration

---
+++ src/main/java/com/chomoncik/clinic/util/ClockConfiguration.java
@@ -0,0 +1,17 @@
+package com.chomoncik.clinic.util;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+}
